fdb9e80ef0d3ea3166640e86122198f9da5d5092
ArthurMizokami <arthurtm154@gmail.com>
2025-03-06 08:52:09 -0300

    Versão 1.0.1 (feat confirmação de devolução)

--- js/app.js
+++ js/app.js
@@ -4,13 +4,31 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')){ 
+        const confirmacao = confirmarDevolucao();
+        if (confirmacao) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.textContent = 'Alugar';
         botao.classList.remove('dashboard__item__button--return');
+        }
         } else { 
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
-    }
+        }
+        
+}
 
+function confirmarDevolucao() {
+    const resposta = prompt('Confirme o devolvimento do jogo com SIM ou NÃo para cancelar:').toLowerCase();
+
+    if (resposta === 'sim') {
+        alert ('Jogo devolvido!');
+        return true;
+    } else if (resposta === 'não' || resposta === 'nao') {
+        alert ('Devolução cancelada!');
+        return false;
+    } else {
+        alert ('Resposta inválida! Digite "SIM" ou "NÃO".'); 
+        return null;
+    }
 }
